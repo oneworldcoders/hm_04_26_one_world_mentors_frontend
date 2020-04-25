c17c15c49604142c470e56e36490d3f3b5e51160
Li-Hsuan <lihsuanlung@gmail.com>
2020-04-25 15:41:55 -0400

    Rebase from develop; remove unused css classes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,5 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="menu">
-        <div className="logo">
-          Mentors
-        </div>
-        <div className="action">
-          Become a Mentor
-        </div>
-        <a className="link" href="/register">
-          Register
-        </a>
-        <a className="link" href="/login">
-          Login
-        </a>
-        <a className="link" href="/about">
-          About
-        </a>
-      </div>
-      <div className="container">
-        <div className="left">
-          <div className="tagline">
-            Interested in becoming a Software Developer in months?
-          </div>
-          <div className="subtitle">
-            Register and meet a mentor.
-          </div>
-        </div>
-        <div className="right">
-        </div>
-      </div>
-    </div>
-  );
-}
 
 export default App;
--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import './welcome.css';
 
 export default function welcome() {
   return (
-    <div>
-      <h1>Hello welcome to OWC Mentors frontend app</h1>
+    <div className="welcome">
+      <div className="menu">
+        <div className="logo">
+          Mentors
+        </div>
+        <div className="action">
+          Become a Mentor
+        </div>
+        <a className="link" href="/register">
+          Register
+        </a>
+        <a className="link" href="/login">
+          Login
+        </a>
+        <a className="link" href="/about">
+          About
+        </a>
+      </div>
+      <div className="hero">
+        <div className="left">
+          <div className="tagline">
+            Interested in becoming a Software Developer in months?
+          </div>
+          <div className="subtitle">
+            Register and meet a mentor.
+          </div>
+        </div>
+        <div className="right">
+        </div>
+      </div>
     </div>
   )
 }
