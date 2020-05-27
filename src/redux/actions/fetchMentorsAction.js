ea51c60d8c235f5214e17032cbd068d6098a8f6e
kenneth051 <kjdumba@gmail.com>
2020-05-27 23:49:30 +0200

    add social changes

--- src/redux/actions/fetchMentorsAction.js
+++ src/redux/actions/fetchMentorsAction.js
@@ -3,13 +3,14 @@ import { FETCH_MENTORS } from "./types";
 
 
 const baseUrl = "https://owc-mentor-backend.herokuapp.com";
-const token = localStorage.getItem('jwtToken');
+
+
+export const fetchMentors = () => dispatch => {
+  const token = localStorage.getItem('jwtToken');
 const headers = {
   'Content-Type': 'application/json',
   'Authorization': `Bearer ${token}`,
 };
-
-export const fetchMentors = () => dispatch => {
   axios
     .get(`${baseUrl}/mentors`, {headers})
     .then(res =>
